7095d849c1ffa4b3c0718068666cabaa47416a01
Scarlett <scarlettroyle@hotmail.co.uk>
2022-02-17 14:10:26 +0100

    Added calcs for uncs in luminosity, added appropriate conversions in (log(solar), solar radius)

--- download_gaia_SR.py
+++ download_gaia_SR.py
@@ -6,7 +6,8 @@ class GaiaDataset:
     Use this class to fetch GAIA dataset from ESA.
     """
 
-    def __init__(self, query=None, table="gaiaedr3.gaia_source", filename="gaiaedr3"):
+    def __init__(self, query=None, table="gaiadr2.gaia_source", filename="gaiadr2"):
+        #table="gaiaedr3.gaia_source", filename="gaiaedr3"):
         self.query = query
         self.filename = filename
         self.table = table
@@ -23,14 +24,19 @@ class GaiaDataset:
         """
         Default query used only if no query supplied by the user.
         """
+        
+        # Retrieve the following columns from the Gaia data. Including T_eff, R, L and their uncs.
 
         self.query = f"""
         select source_id, ra, ra_error, dec, dec_error, parallax, parallax_error, pmra, pmra_error, pmdec, pmdec_error,
         dr2_radial_velocity, dr2_radial_velocity_error from {self.table} where source_id is not null and ra is 
         not null and ra_error is not null and dec is not null and dec_error is not null and parallax is not null and 
         parallax_error is not null and pmra is not null and pmra_error is not null and pmdec is not null and 
-        pmdec_error is not null and dr2_radial_velocity is not null and dr2_radial_velocity_error is not null
+        pmdec_error is not null and dr2_radial_velocity is not null and dr2_radial_velocity_error is not null        
         """
+        #, teff_val, teff_percentile_lower, teff_percentile_upper, radius_val, radius_percentile_lower, radius_percentile_upper, lum_val, lum_percentile_lower, lum_percentile_upper,
+        
+        #and teff_val is not null and teff_percentile_lower is not null and teff_percentile_upper  is not null and radius_val  is not null and radius_percentile_lower  is not null and radius_percentile_upper  is not null and lum_val  is not null and lum_percentile_lower  is not null and lum_percentile_upper  is not null
 
     def get_gaia(self, query=None):
         """
@@ -45,5 +51,7 @@ class GaiaDataset:
         if self.filename[-4:] != ".csv":
             self.filename = self.filename + ".csv"
 
-        Gaia.launch_job_async(self.query).get_results().to_pandas().to_csv("data/initial_datasets/" + self.filename,
-                                                                           index=False)
+        Gaia.launch_job_async(self.query).get_results().to_pandas().to_csv("data/initial_datasets/" + self.filename, index=False)
+        #~/Scarlett/OneDrive - Liverpool John Moores University/SEPHI_data/
+        #data/initial_datasets/
+        #TODO: the above is messing up
