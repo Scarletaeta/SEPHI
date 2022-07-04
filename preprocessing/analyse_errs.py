08a8fbb82ffb3e7b84f5d019462f7de2a1260c9a
Scarlett <scarlettroyle@hotmail.co.uk>
2022-07-04 14:27:39 +0100

    Adding crossmatching checks in

--- preprocessing/analyse_errs.py
+++ preprocessing/analyse_errs.py
@@ -14,6 +14,7 @@ def calc_percent_errs(values, errs):
     percent_errs =  np.absolute( np.multiply(errs, values**(-1)) ) * 100
     return percent_errs
 
+
 def mean_percent_errs(data):
     """
     data = data frame: values, errs1, errs2
@@ -34,4 +35,27 @@ def mean_percent_errs(data):
     means = ( calc_percent_errs(values, errs1) + calc_percent_errs(values, errs2) ) / 2
     return means
 
+
 #def classify():
+
+
+def get_best_values(df, param):
+    """
+    Retrieves the parameter value from the exoplanets df that corresponds to the flagged 'best' parameter.
+    
+    df = input data frame (exoplanets)
+    param = the parameter (e.g. 'st_age'
+    """
+    
+    param = param + '_best'
+    best_values = np.zeros(len(df), dtype=np.float64)
+    
+    for m in range(len(df)):
+        if df[param].iloc[m] != df[param].iloc[m]:
+            
+            best_values[m] = np.nan
+            
+        else:
+            best_values[m] = df[ df[param].iloc[m] ].iloc[m]
+            
+    return best_values
