3affa982887be554ba558b151ef422e340a4c865
Scarlett <scarlettroyle@hotmail.co.uk>
2022-07-26 18:10:05 +0200

    Committing before changing the error analysis to include values with no uncertainties

--- preprocessing/analyse_errs.py
+++ preprocessing/analyse_errs.py
@@ -33,28 +33,35 @@ def mean_percent_errs(data):
     percent_errs2 =  np.absolute( np.multiply(errs2, values**(-1)) ) * 100
     
     means = ( calc_percent_errs(values, errs1) + calc_percent_errs(values, errs2) ) / 2
+    
+    #TODO: If the value is not nan but the uncertainties are, then give it a mean unc of 1 million so that it will get picked if no others are available
+    
     return means
-
-
-#def classify():
 
 
 def get_best_values(df, param):
     """
     Retrieves the parameter value from the exoplanets df that corresponds to the flagged 'best' parameter.
     
-    df = input data frame (exoplanets)
-    param = the parameter (e.g. 'st_age'
+    df = input data frame (exoplanets) that contains columns with string identifiers for the heading of the column containing the 'best' version of a particular parameter for each exoplanet 
+    param = the parameter (e.g. 'st_age')
     """
     
+    # The column name for the list/df of best parameters (e.g. 'st_age_best') in the exoplanets df:
     param = param + '_best'
+    
+    # Empty array for the 'best' parameters:
     best_values = np.zeros(len(df), dtype=np.float64)
     
+    # For all items in the column names 'best'
     for m in range(len(df)):
+        
+        # If entry m is np.nan make the best value np.nan:
         if df[param].iloc[m] != df[param].iloc[m]:
-            
+            # one of the properties of np.nan is that np.nan != np.nan
             best_values[m] = np.nan
-            
+        
+        # If the best values contain a string (not nan) then go to the corresponding column (with the heading 'param') and get the value listed for that exoplanet (index m):
         else:
             best_values[m] = df[ df[param].iloc[m] ].iloc[m]
             
